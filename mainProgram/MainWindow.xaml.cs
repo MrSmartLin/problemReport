c04b97dc1af66a0f2656164c43f004969d90e1c3
MrSmartLin <490650157@qq.com>
2019-07-27 23:15:55 +0800

    haha

--- mainProgram/MainWindow.xaml.cs
+++ mainProgram/MainWindow.xaml.cs
@@ -23,6 +23,25 @@ namespace mainProgram
         public MainWindow()
         {
             InitializeComponent();
+            TreeViewItem item1 = new TreeViewItem() { Header = "节点一" };
+            TreeViewItem item11 = new TreeViewItem() { Header = "节点1-1" };
+            item11.Items.Add("aaaa");
+            item11.Items.Add("bbbb");
+            item11.Items.Add("cccc");
+            item11.Items.Add("dddd");
+
+
+            item1.Items.Add(item11);
+            item1.Items.Add("cccc");
+            item1.Items.Add("dddd");
+
+            TreeViewItem item2 = new TreeViewItem() { Header = "节点二" };
+            item2.Items.Add("aaaa");
+            item2.Items.Add("bbbb");
+            item2.Items.Add("cccc");
+            item2.Items.Add("dddd");
+            treeViewTotalProb.Items.Add(item1);
+            treeViewTotalProb.Items.Add(item2);
         }
     }
 }
